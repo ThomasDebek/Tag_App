d48483b22e34edd4ed9696bf7c6d449d00945ddc
ThomasDebek <tom.debek@gmail.com>
2026-03-09 22:15:00 +0100

    js: modification js

--- app/javascript/controllers/tag_controller.js
+++ app/javascript/controllers/tag_controller.js
@@ -4,6 +4,8 @@ import Tagify from '@yaireo/tagify'
 // Connects to data-controller="tag"
 export default class extends Controller {
   connect() {
-    this.tagify = new Tagify(this.element, {})
+    this.tagify = new Tagify(this.element, {
+      originalInputValueFormat: valuesArr => valuesArr.map(item => item.value).join(',')
+    })
   }
 }
